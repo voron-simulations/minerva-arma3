538bf3979c07bbad4cdfe0352c7328c04995cae7
Oleg V. Kozlyuk <elquentaro@gmail.com>
2025-10-29 18:43:52 +0100

    Fixed build, updated deps

--- build.rs
+++ build.rs
@@ -1,5 +1,20 @@
-extern crate prost_build;
+extern crate glob;
+extern crate tonic_prost_build;
 
-fn main() {
-    prost_build::compile_protos(&["proto/*.proto"], &["proto/"]).unwrap();
+use glob::glob;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let manifest_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
+    let proto_dir = manifest_dir.join("proto");
+
+    let mut proto_files = Vec::new();
+    for p in glob(proto_dir.join("*.proto").to_str().unwrap())? {
+        proto_files.push(p?);
+    }
+
+    let builder = tonic_prost_build::configure();
+
+    builder.compile_protos(&proto_files, &[proto_dir])?;
+
+    Ok(())
 }
